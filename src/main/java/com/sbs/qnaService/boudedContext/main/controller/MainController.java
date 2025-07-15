66b7686af297108d38a97d8b704cf33b1c46253a
SangWon7242 <gotkdqja@gmail.com>
2025-07-15 19:53:14 +0900

    MainController

---
+++ src/main/java/com/sbs/qnaService/boudedContext/main/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.sbs.qnaService.boudedContext.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // 컨트롤러로서의 역할을 부여
+public class MainController {
+  @GetMapping("/main") // "/main" 경로로 GET 요청을 처리
+  @ResponseBody // 응답결과를 본문에 직접 작성
+  public String showMain() {
+    return "안녕하세요. 스프링부트에 오신 것을 환영합니다.";
+  }
+}
