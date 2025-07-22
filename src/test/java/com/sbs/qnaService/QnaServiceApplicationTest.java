6d6178b9cfa09a7643081d521cfde62fc7f4d900
SangWon7242 <gotkdqja@gmail.com>
2025-07-22 20:41:07 +0900

    @Transactional을 붙임으로 하나의 트랜잭션으로 작업

--- src/test/java/com/sbs/qnaService/QnaServiceApplicationTest.java
+++ src/test/java/com/sbs/qnaService/QnaServiceApplicationTest.java
@@ -22,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ActiveProfiles("test")  // 테스트 프로파일을 사용하여 테스트 환경 설정
-@Transactional
 @Rollback(false)
 class QnaServiceApplicationTest {
 
@@ -237,13 +236,17 @@ class QnaServiceApplicationTest {
 
 	@Test
 	@DisplayName("질문을 통해 답변 조회")
+	@Transactional
 	void t11() {
+		// SELECT * FROM question WHERE id = 2;
 		Optional<Question> oq = questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 		
 		// 질문에 있는 답변 목록을 가져옴
+		// SELECT * FROM answer WHERE question_id = 2;
 		List<Answer> answerList = q.getAnswerList();
+
 		// 질문에 대한 답변이 1개가 저장되어 있으므로, answerList의 크기는 1이어야 한다.
 		assertEquals(1, answerList.size());
 		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
