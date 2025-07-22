14606afcbec98fccf52e0ee8b763d15f4b4e81cb
SangWon7242 <gotkdqja@gmail.com>
2025-07-22 21:23:49 +0900

    ToString 으로 객체 정보를 조금더 편하게 보기

--- src/main/java/com/sbs/qnaService/boudedContext/question/entity/Question.java
+++ src/main/java/com/sbs/qnaService/boudedContext/question/entity/Question.java
@@ -4,6 +4,7 @@ import com.sbs.qnaService.boudedContext.answer.entity.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 @Setter
 @Entity // 엔티티로서의 역할을 부여
 // 아래 클래스와 1:1 매칭되는 테이블이 DB에 없다면, 자동으로 생성
+@ToString
 public class Question {
   @Id // Primary Key로 지정
   @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment 설정
