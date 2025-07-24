22b11d79c60294432fad7a3e459675f4874242e4
SangWon7242 <gotkdqja@gmail.com>
2025-07-24 19:39:43 +0900

    질문 상세보기 페이지

--- src/main/java/com/sbs/qnaService/boudedContext/question/controller/QuestionController.java
+++ src/main/java/com/sbs/qnaService/boudedContext/question/controller/QuestionController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -25,5 +26,10 @@ public class QuestionController {
     model.addAttribute("questionList", questionList);
     
     return "question_list";
+  }
+
+  @GetMapping("/question/detail/{id}")
+  public String detail(Model model, @PathVariable("id") Integer id) {
+    return "question_detail";
   }
 }
