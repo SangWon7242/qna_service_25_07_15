192cec292d7f131a7559ddca5d9e58ebdfd79d0d
SangWon7242 <gotkdqja@gmail.com>
2025-07-31 19:53:06 +0900

    각 페이지별로 로그인 필요/불필요를 명시

--- src/main/java/com/sbs/qnaService/base/config/security/SecurityConfig.java
+++ src/main/java/com/sbs/qnaService/base/config/security/SecurityConfig.java
@@ -17,10 +17,21 @@ public class SecurityConfig {
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     // http.formLogin : 스프링 시큐리티가 사용하는 폼 로그인 기능을 활성화
-
-    http.formLogin((formLogin) -> formLogin
-        .loginPage("/user/login") // 기본 제공되는 페이지 대신에 login 템플릿을 폼으로 사용
-        .defaultSuccessUrl("/")); // 로그인 성공 후 리다이렉트 할 기본 경로
+    // 아래 설정은 로그인시에만 해당 페이지에 접근이 가능하게 설정
+    http.authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/question/list").permitAll() // 질문 관련 페이지 접근 허용
+              .requestMatchers("/question/detail/**").permitAll() // 질문 상세 페이지 접근 허용
+              .requestMatchers("/user/signup").permitAll() // 회원가입 페이지 접근 허용
+              .requestMatchers("/user/login").permitAll() // 로그인 페이지 접근 허용
+              .requestMatchers("/user/logout").permitAll() // 로그아웃 페이지 접근 허용
+              .requestMatchers("/style.css").permitAll() // 스타일시트 접근 허용
+              .requestMatchers("/").permitAll()
+              .anyRequest().authenticated(); // 나머지 요청은 인증 필요
+        })
+        .formLogin((formLogin) -> formLogin
+            .loginPage("/user/login") // 기본 제공되는 페이지 대신에 login 템플릿을 폼으로 사용
+            .loginProcessingUrl("/user/login") // 시큐리티한테 로그인 폼 처리 url을 알려줌
+            .defaultSuccessUrl("/")); // 로그인 성공 후 리다이렉트 할 기본 경로
 
     return http.build(); // 기본설정이 적용
   }
@@ -32,7 +43,7 @@ public class SecurityConfig {
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-  
+
   // AuthenticationManager : 인증을 처리하는 매니저
   // AuthenticationConfiguration : 인증 관련 설정을 제공
   // 로그인시 인증 매니저를 통해 사용자 처리
