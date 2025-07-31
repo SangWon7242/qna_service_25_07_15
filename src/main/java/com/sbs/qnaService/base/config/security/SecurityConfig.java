1cd8b46c1e184a8cf23668684e880d0221197875
SangWon7242 <gotkdqja@gmail.com>
2025-07-31 19:26:36 +0900

    step1 : 시큐리티 로그인 폼을 login_form으로 변경

--- src/main/java/com/sbs/qnaService/base/config/security/SecurityConfig.java
+++ src/main/java/com/sbs/qnaService/base/config/security/SecurityConfig.java
@@ -14,19 +14,19 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // http.formLogin : 스프링 시큐리티가 사용하는 폼 로그인 기능을 활성화
 
-    http
-        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-            .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-    ;
+    http.formLogin((formLogin) -> formLogin
+        .loginPage("/user/login") // 기본 제공되는 페이지 대신에 login 템플릿을 폼으로 사용
+        .defaultSuccessUrl("/")); // 로그인 성공 후 리다이렉트 할 기본 경로
 
     return http.build(); // 기본설정이 적용
   }
 
   @Bean
-  // 비밀번호 암호화 검증
-  // 인증 시 암호화 된 비밀번호와 비교
-  // BCrypt 암호화 알고리즘으로 비밀번호를 암호화
+    // 비밀번호 암호화 검증
+    // 인증 시 암호화 된 비밀번호와 비교
+    // BCrypt 암호화 알고리즘으로 비밀번호를 암호화
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
