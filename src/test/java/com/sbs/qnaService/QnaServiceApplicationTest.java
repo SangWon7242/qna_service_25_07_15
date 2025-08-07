6cb273abc5144fd1e5cd6648a219a6a54501c903
SangWon7242 <gotkdqja@gmail.com>
2025-08-07 19:21:49 +0900

    추천 테스트 데이터 수정

--- src/test/java/com/sbs/qnaService/QnaServiceApplicationTest.java
+++ src/test/java/com/sbs/qnaService/QnaServiceApplicationTest.java
@@ -84,12 +84,15 @@ class QnaServiceApplicationTest {
 
     q1.addVoter(user1);
     q1.addVoter(user2);
+    questionRepository.save(q1);
 
     q2.addVoter(user1);
     q2.addVoter(user2);
+    questionRepository.save(q2);
 
     a1.addVoter(user1);
     a1.addVoter(user2);
+    answerRepository.save(a1);
 	}
 
 	@Test
