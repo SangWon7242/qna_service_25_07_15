5dd039f8794fe7ecbcf3e263168131352d288137
SangWon7242 <gotkdqja@gmail.com>
2025-08-07 19:31:32 +0900

    답변 추천

--- src/main/java/com/sbs/qnaService/boudedContext/question/controller/QuestionController.java
+++ src/main/java/com/sbs/qnaService/boudedContext/question/controller/QuestionController.java
@@ -117,6 +117,7 @@ public class QuestionController {
   public String questionVote(Principal principal, @PathVariable("id") Integer id) {
     Question question = questionService.getQuestion(id);
     SiteUser siteUser = userService.getUser(principal.getName());
+
     questionService.vote(question, siteUser);
     return String.format("redirect:/question/detail/%s", id);
   }
